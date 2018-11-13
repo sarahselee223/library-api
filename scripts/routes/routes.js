4453614cc445cbf47593523d8b5fcf0203d2f174
rogerwschmidt <rogerschmidt@gmail.com>
2018-11-13 15:18:03 -0800

    feedback

--- scripts/routes/routes.js
+++ scripts/routes/routes.js
@@ -8,6 +8,8 @@ const authorctrl = require('../controls/author_controls.js')
 router.use('/:id/authors', authorRouter)
 
 router.get('/', bookctrl.getAll)
+// TODO: complete this `getOne` route
+router.get('/:id', bookctrl.getOne)
 router.post('/', bookctrl.create)
 router.delete('/:id', bookctrl.deleteOne)
 router.put('/:id', bookctrl.editOne)
